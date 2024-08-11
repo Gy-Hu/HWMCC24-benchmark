ca45db6ffadf0476a6d2f93962e1536d393a3ef3
GaryHU <swe1609011@gmail.com>
2024-08-12 00:14:11 +0800

    update bv version of rocket's btor

--- R-map/ILA_Rocket/gen_btor.sh
+++ R-map/ILA_Rocket/gen_btor.sh
@@ -3,6 +3,10 @@
 # Define the base directory as the current working directory plus "verification"
 base_dir="$(pwd)/verification"
 
+# Set the maximum number of parallel jobs
+max_jobs=2
+current_jobs=0
+
 # Iterate over each subdirectory within the base directory
 for dir in "$base_dir"/*/; do
   # Check if the directory contains the file gen_btor.ys
@@ -10,12 +14,21 @@ for dir in "$base_dir"/*/; do
     echo "Executing yosys gen_btor.ys in $dir"
     # Change into the directory and execute yosys in the background
     (cd "$dir" && yosys gen_btor.ys) &
+
+    # Increment the count of current jobs
+    current_jobs=$((current_jobs + 1))
+
+    # If the current number of jobs reaches max_jobs, wait for one to finish
+    if [[ $current_jobs -ge $max_jobs ]]; then
+      wait -n  # Wait for any background process to finish
+      current_jobs=$((current_jobs - 1))
+    fi
   else
     echo "No gen_btor.ys file found in $dir"
   fi
 done
 
-# Wait for all background processes to complete
+# Wait for all remaining background processes to complete
 wait
 
 echo "All yosys processes completed."
